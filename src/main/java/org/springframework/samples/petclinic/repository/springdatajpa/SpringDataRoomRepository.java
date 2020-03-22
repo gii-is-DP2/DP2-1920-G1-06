c39704ed271de495d0cacfdf09bc70b8d0df14c1
Pablo Muñoz <pabmundel@alum.us.es>
2020-03-22 16:21:28 +0100

    correccion error BD

--- src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataRoomRepository.java
@@ -1,3 +1,4 @@
+
 package org.springframework.samples.petclinic.repository.springdatajpa;
 
 import org.springframework.data.jpa.repository.Query;
@@ -6,9 +7,10 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.samples.petclinic.model.Room;
 import org.springframework.samples.petclinic.repository.RoomRepository;
 
-public interface SpringDataRoomRepository extends RoomRepository, Repository<Room,Integer>{
+public interface SpringDataRoomRepository extends RoomRepository, Repository<Room, Integer> {
 
-	@Query("SELECT r FROM Rooms r WHERE r.id =:id")
+	@Override
+	@Query("SELECT r FROM Room r WHERE r.id =:id")
 	Room findRoomById(@Param("id") int id);
-	
+
 }
--- src/main/java/org/springframework/samples/petclinic/web/RoomController.java
+++ src/main/java/org/springframework/samples/petclinic/web/RoomController.java
@@ -1,3 +1,4 @@
+
 package org.springframework.samples.petclinic.web;
 
 import java.util.Map;
@@ -5,7 +6,6 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Property;
 import org.springframework.samples.petclinic.model.Room;
 import org.springframework.samples.petclinic.service.PropertyService;
 import org.springframework.samples.petclinic.service.RoomService;
@@ -18,60 +18,56 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RoomController {
-	
-	private static final String VIEWS_ROOMS_CREATE_OR_UPDATE_FORM = "rooms/createOrUpdateRoomForm";
-	
-	private PropertyService propertyService;
-	
-	private final RoomService roomService;
-	
+
+	private static final String	VIEWS_ROOMS_CREATE_OR_UPDATE_FORM	= "rooms/createOrUpdateRoomForm";
+
+	private PropertyService		propertyService;
+
+	private final RoomService	roomService;
+
+
 	@Autowired
-	public RoomController(RoomService roomService) {
+	public RoomController(final RoomService roomService) {
 		this.roomService = roomService;
 	}
-	
+
 	//CREACION DE HABITACION ------------------------------------------------------------------------
 
-	@GetMapping(value = "/rooms/{propertyId}/new")
-	public String initCreationForm(@PathVariable("propertyId") int propertyId, Map<String, Object> model) {
+	@GetMapping(value = "/rooms/new")
+	public String initCreationForm(final Map<String, Object> model) {
 		Room room = new Room();
 		model.put("room", room);
-		Property property = propertyService.findPropertyById(propertyId);
-		room.setProperty(property);
-		return VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
+		return RoomController.VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
 	}
 
-	@PostMapping(value = "/rooms/{propertyId}/new")
-	public String processCreationForm(@Valid Room room, BindingResult result) {
+	@PostMapping(value = "/rooms/new")
+	public String processCreationForm(@Valid final Room room, final BindingResult result) {
 		if (result.hasErrors()) {
-			return VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
-		}
-		else {			
-			this.roomService.saveRoom(room);			
+			return RoomController.VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
+		} else {
+			this.roomService.saveRoom(room);
 			return "redirect:/rooms/" + room.property.getId();
 		}
 	}
-	
+
 	//MODIFICACION DE HABITACION -----------------------------------------------------------------------
-	
+
 	@GetMapping(value = "/rooms/{roomId}/edit")
-	public String initUpdateRoomForm(@PathVariable("roomId") int roomId, Model model) {
+	public String initUpdateRoomForm(@PathVariable("roomId") final int roomId, final Model model) {
 		Room room = this.roomService.findRoomById(roomId);
 		model.addAttribute(room);
-		return VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
+		return RoomController.VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
 	}
 
 	@PostMapping(value = "/rooms/{roomId}/edit")
-	public String processUpdateRoomForm(@Valid Room room, BindingResult result,
-			@PathVariable("roomId") int roomId) {
+	public String processUpdateRoomForm(@Valid final Room room, final BindingResult result, @PathVariable("roomId") final int roomId) {
 		if (result.hasErrors()) {
-			return VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
-		}
-		else {
+			return RoomController.VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
+		} else {
 			room.setId(roomId);
 			this.roomService.saveRoom(room);
 			return "redirect:/rooms/{roomId}";
 		}
 	}
-	
+
 }
